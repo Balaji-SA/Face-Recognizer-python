7e58e7c694619e31ff7b8ea16a7e53cfaba44440
Balaji SA <50614674+Balaji-SA@users.noreply.github.com>
2019-05-26 14:03:46 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -36,5 +36,4 @@ recognizer.train(faces, np.array(ids))
 
 recognizer.write('trainer/trainer.yml') 
 
-# Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
